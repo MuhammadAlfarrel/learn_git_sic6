dd68a01c4be594a1d9be066415e571f996810a7c
Muhammad Alfarrel Arya Mahardika <alfarrelmahardika@gmail.com>
2025-02-08 10:45:05 +0700

    func math in python

---
+++ main.py
@@ -0,0 +1,17 @@
+def penjumlahan(a,b):
+    return a+b
+
+def pengurangan(a,b):
+    return a-b
+
+def perkalian(a,b):
+    return a*b
+
+def pembagian(a,b):
+    return a/b
+
+a = 4
+b =2
+
+result = penjumlahan(a,b)
+print(result)
